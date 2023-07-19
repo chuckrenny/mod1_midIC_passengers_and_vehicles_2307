376284a8757c5d17e494c321801909ede40052f4
Charles Ren <cren04@syr.edu>
2023-07-19 08:58:17 -0700

    test and feat: add_vehicles to the park

--- lib/park.rb
+++ lib/park.rb
@@ -1,10 +1,15 @@
 class Park
     attr_reader :name,
-                :admission
+                :admission,
+                :vehicles
 
     def initialize(name, admission)
         @name = name
         @admission = admission
+        @vehicles = []
     end
 
+    def add_vehicle(vehicle)
+        @vehicles << vehicle
+    end
 end
--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -3,17 +3,30 @@ require 'spec_helper'
 RSpec.describe Park do
     before(:each) do
         @park = Park.new("Veterans Park", 10)
-        @vehicle = Vehicle.new("2001", "Honda", "Civic")
+        @vehicle1 = Vehicle.new("2001", "Honda", "Civic")
+        @vehicle2 = Vehicle.new("2022", "Tesla", "Model Y")
 
         @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
         @jude = Passenger.new({"name" => "Jude", "age" => 20})
         @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+        @daniel = Passenger.new({"name" => "Daniel", "age" => 22})
     end
 
     it 'can initialize and access attributes' do
         expect(@park).to be_a(Park)
         expect(@park.name).to eq("Veterans Park")
         expect(@park.admission).to eq(10)
+        expect(@park.vehicles).to eq([])
     end
 
+    it 'can add vehicles to the park' do
+        @vehicle1.add_passenger(@charlie)
+        @vehicle1.add_passenger(@jude)
+        @vehicle2.add_passenger(@taylor)
+        @vehicle2.add_passenger(@daniel)
+
+        @park.add_vehicle(@vehicle1)
+        @park.add_vehicle(@vehicle2)
+        expect(@park.vehicles).to eq([@vehicle1, @vehicle2])
+    end
 end
